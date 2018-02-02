d0ffd83cdf97f3c613843284d382174de3d4604f
Jake Humphrey <jakehumphrey18@gmail.com>
2018-02-02 15:37:18 -0600

    Adding DocFlux Document

---
+++ src/SpreadSheetDocument.js
@@ -0,0 +1,47 @@
+import saveAs from 'save-as';
+import XLSX from 'xlsx';
+import { Document } from '@harvest-profit/doc-flux';
+import Parser from './Parser';
+
+/**
+ * The generated document object.  Just call download
+ * @module GeneratedDocument
+ */
+class GeneratedDocument {
+  constructor(doc, settings) {
+    const bookType = settings.type || 'csv';
+    const filename = settings.name || 'Untitled';
+    const wopts = { bookType, type: 'array' };
+    this.filename = filename;
+    this.extension = bookType;
+    this.doc = XLSX.write(doc, wopts);
+  }
+
+  /**
+   * Downloads the document
+   * @function
+   */
+  download() {
+    /* the saveAs call downloads a file on the local machine */
+    saveAs(new Blob([this.doc], { type: 'application/octet-stream' }), `${this.filename}.${this.extension}`);
+  }
+}
+
+/**
+ * The DocFlux Document (exported as Document).
+ * @module SpreadSheetDocument
+ */
+export default class SpreadSheetDocument extends Document {
+  static documentSettings() {
+    return {};
+  }
+
+  static createDocument(doc, props) {
+    return new GeneratedDocument(doc, this.documentSettings(props));
+  }
+  static createBuilder() {
+    return XLSX.utils.book_new();
+  }
+
+  static parser = Parser;
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import Table from './DOMComponents/Table';
-import TableBody from './DOMComponents/TableBody';
-import TableHeader from './DOMComponents/TableHeader';
-import TableHeaderItem from './DOMComponents/TableHeaderItem';
-import TableRow from './DOMComponents/TableRow';
-import TableItem from './DOMComponents/TableItem';
-import TableName from './DOMComponents/TableName';
+import Parser from './Parser';
+import Document from './SpreadSheetDocument';
 
-export default {
-  table: Table,
-  thead: TableHeader,
-  tbody: TableBody,
-  th: TableHeaderItem,
-  td: TableItem,
-  tr: TableRow,
-  tname: TableName,
+export {
+  Parser,
+  Document,
 };
